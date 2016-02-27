bb878e852b545f2243115af4a2816b526650cd16
threehams <threehams@gmail.com>
2016-02-27 14:22:07 -0800

    REV-7 Correct some tests

--- src/reducers/ui-reducer.spec.js
+++ src/reducers/ui-reducer.spec.js
@@ -6,21 +6,20 @@ chai.use(chaiImmutable);
 import uiReducer from './ui-reducer';
 import {setState} from '../actions/initial-actions';
 import {toggleExpand} from '../actions/inventory-actions';
+import UiRecord from '../records/ui-record';
 
 describe('uiReducer', function() {
   describe('SET_STATE', function() {
     it('returns the default UI state', function() {
       const initial = undefined;
       const action = setState(fromJS({}));
-      expect(uiReducer(initial, action)).to.equal(fromJS({
-        inventoryExpandedById: {}
-      }));
+      expect(uiReducer(initial, action)).to.equal(new UiRecord());
     });
   });
 
   describe('INVENTORY_TOGGLE_EXPAND', function() {
     it('toggles the expanded state', function() {
-      const initial = fromJS({ inventoryExpandedById: {} });
+      const initial = new UiRecord();
       const action = toggleExpand('1');
       expect(uiReducer(initial, action)).to.equal(fromJS({
         inventoryExpandedById: {
