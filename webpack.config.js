805665cffe84f9c6cc9f091e509153ab29074341
threehams <threehams@gmail.com>
2016-05-24 09:26:22 -0700

    REV-129 Replace sass with css

--- webpack.config.js
+++ webpack.config.js
@@ -87,8 +87,8 @@ module.exports = {
         include: path.join(__dirname, 'src')
       },
       {
-        test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader'
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
       },
       {
         test: /\.json$/,
