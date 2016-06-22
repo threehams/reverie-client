c179d6d7ee533cde1342badc3d1b9b76b1e54438
threehams <threehams@gmail.com>
2016-06-22 08:56:08 -0700

    REV-135 Get most typings working (react-redux is a problem)

--- src/components/DropdownArrow.spec.tsx
+++ src/components/DropdownArrow.spec.tsx
@@ -8,11 +8,11 @@ import { DropdownArrow, Icon } from './';
 describe('DropdownArrow', function() {
   it('shows a right arrow when not expanded', function() {
     const element = shallow(<DropdownArrow />);
-    expect(element.find(Icon).props().name).to.equal('icon-arrow-right');
+    expect(element.find(Icon).prop('name')).to.equal('icon-arrow-right');
   });
 
   it('shows a down arrow when expanded', function() {
     const element = shallow(<DropdownArrow expanded />);
-    expect(element.find(Icon).props().name).to.equal('icon-arrow-down');
+    expect(element.find(Icon).prop('name')).to.equal('icon-arrow-down');
   });
 });
